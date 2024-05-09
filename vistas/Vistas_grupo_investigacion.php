9eed1f3f06ff07f2f64f6a6b3153355e16ebaf66
Felipe Rodríguez <Danitronick22@gmail.com>
2024-05-09 13:56:18 -0500

    Diseño Visualización grupos

---
+++ vistas/Vistas_grupo_investigacion.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vistas de Grupo de Investigación</title>
+    <link rel="stylesheet" href="../css/vistas.css">
+</head>
+<body>
+    <div id="Vista_grupo" class="conteiner">
+    <h1 class="title">Vistas de Grupo de investigacion</h1>
+    <table class="table table_title table-hover">
+        <thead class="tablee table-warning table-bordered b3">
+        <tr>
+            <th class="th_color" scope="col">Cedula</th>
+            <th class="th_color" scope="col">Nombre</th>
+            <th class="th_color" scope="col">Correo</th>
+            <th class="th_color" scope="col">Telefono</th>
+            <th style="padding-right: 11px;" class="th_color" scope="col">Direccion</th>
+        </tr>
+        </thead>
+    </table>
+    <div class="content">
+        <form class="consultar" action="">
+        <!-- Tabla de clientes -->
+        <div id="clientes" style="display: block;">
+        <table class="table table-hover">
+            <tbody>
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">2</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">3</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">2</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">3</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">2</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">3</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">2</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            <tr class="tr-row" style="font-size: smaller">
+                <th scope="row">3</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr> 
+            </tbody>
+        </table>
+        </div>
+    </div>  
+    </div>
+
+    <script src="../js/scripts.js"></script>
+</body>
+</html>

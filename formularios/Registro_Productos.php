6121dafba542e253e6153ae4a27f5d2926cc8e00
Felipe Rodríguez <Danitronick22@gmail.com>
2024-05-19 20:52:25 -0500

    Commit productos

--- formularios/Registro_Productos.php
+++ formularios/Registro_Productos.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Historial Productos</title>
   <link rel="stylesheet" href="../css/registroGrupo.css">
 </head>
 <body>
